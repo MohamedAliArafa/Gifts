cc62845306af59fc832142b60a4576cb6ea3becf
NoraAmer <noraamer2015@gmail.com>
2016-03-22 15:29:00 +0200

    22-3-2016

--- app/src/main/java/com/zeowls/gifts/LoginActivity.java
+++ app/src/main/java/com/zeowls/gifts/LoginActivity.java
@@ -3,9 +3,13 @@ package com.zeowls.gifts;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -19,6 +23,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,6 +34,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.People;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +51,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends FragmentActivity implements LoaderCallbacks<Cursor>, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, OnClickListener, ResultCallback<People.LoadPeopleResult> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -56,12 +70,27 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private UserLoginTask mAuthTask = null;
 
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+
+
+    private static final int SIGNED_IN = 0;
+    private static final int STATE_SIGNING_IN = 1;
+    private static final int STATE_IN_PROGRESS = 2;
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+    private int mSignInProgress;
+    private PendingIntent mSignInIntent;
+
+    private SignInButton mSignInButton;
+    private Button mSignOutButton;
+    private TextView mStatus;
+
+    private  TextView mPersonData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +122,43 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        // Get references to all of the UI views
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mStatus = (TextView) findViewById(R.id.statuslabel);
+
+        // Add click listeners for the button
+        mSignInButton.setOnClickListener(this);
+        mSignOutButton.setOnClickListener(this);
+
+        // Build a GoogleApiClient
+        mGoogleApiClient = buildGoogleApiClient();
+
+        mPersonData = (TextView) findViewById(R.id.person_data);
+
+    }
+
+    private GoogleApiClient buildGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(new Scope("email"))
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 
     private void populateAutoComplete() {
@@ -277,6 +343,137 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mEmailView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mSignInButton.setEnabled(false);
+        mSignOutButton.setEnabled(true);
+
+        // Indicate that the sign in process is complete.
+        mSignInProgress = SIGNED_IN;
+
+        Log.d("Connected","true");
+        Plus.PeopleApi.loadVisible(mGoogleApiClient, null).setResultCallback(this);
+
+        String personName="Unknown";
+        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            personName = currentPerson.getDisplayName();
+            Log.d("Test APi ",personName);
+        }else {
+            Log.d("Test APi ","Null ");
+        }
+
+
+        try {
+            String emailAddress = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            //    String personName = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient).getDisplayName();
+            //String personPhoto = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient).getImage();
+            //    String personId = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient).getId();
+            //   String personGooglePlusProfile = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient).getUrl();
+//
+            mStatus.setText(String.format("Signed In to My App as %s", emailAddress));
+
+
+
+
+            //    Log.d("Person" ,personGooglePlusProfile);
+            mPersonData.setText("zzzzzzzzz");
+            //    mPersonData.setText(personName+"\n"+personId+"\n"+personGooglePlusProfile);
+        } catch (Exception ex) {
+            String exception = ex.getLocalizedMessage();
+            String exceptionString = ex.toString();
+            Log.d("Exc",exceptionString);
+            // Note that you should log these errors in a 'real' app to aid in debugging
+        }
+//        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+//            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+//            String personName = currentPerson.getDisplayName();
+////            String personPhoto = currentPerson.getImage();
+//            String personId = currentPerson.getId();
+//            String personGooglePlusProfile = currentPerson.getUrl();
+//            mPersonData.setText(personName+"\n"+personId+"\n"+personGooglePlusProfile);
+//        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mSignInProgress != STATE_IN_PROGRESS) {
+            mSignInIntent = connectionResult.getResolution();
+            if (mSignInProgress == STATE_SIGNING_IN) {
+                resolveSignInError();
+            }
+        }
+        // Will implement shortly
+        onSignedOut();
+    }
+
+    private void onSignedOut() {
+        // Update the UI to reflect that the user is signed out.
+        mSignInButton.setEnabled(true);
+        mSignOutButton.setEnabled(false);
+        mStatus.setText("Signed out");
+    }
+
+    private void resolveSignInError() {
+        if (mSignInIntent != null) {
+            try {
+                mSignInProgress = STATE_IN_PROGRESS;
+                startIntentSenderForResult(mSignInIntent.getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                mSignInProgress = STATE_SIGNING_IN;
+                mGoogleApiClient.connect();
+            }
+        } else {
+            // You have a play services error -- inform the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                if (resultCode == RESULT_OK) {
+                    mSignInProgress = STATE_SIGNING_IN;
+                } else {
+                    mSignInProgress = SIGNED_IN;
+                }
+
+                if (!mGoogleApiClient.isConnecting()) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!mGoogleApiClient.isConnecting()) {
+            switch (v.getId()) {
+                case R.id.sign_in_button:
+                    mStatus.setText("Signing In");
+                    resolveSignInError();
+                    break;
+                case R.id.sign_out_button:
+                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+                    mGoogleApiClient.disconnect();
+                    mGoogleApiClient.connect();
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onResult(People.LoadPeopleResult loadPeopleResult) {
+
     }
 
 
