9ce78d13ba251803a9da7ea4169f1476579ad4a4
Nezar <Nezarsalehhasan@gmail.com>
2016-03-29 13:23:38 +0200

    27-3-2016

---
+++ app/src/main/java/com/zeowls/gifts/ImageSlider/ScreenSlidePageFragment.java
@@ -0,0 +1,27 @@
+package com.zeowls.gifts.ImageSlider;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zeowls.gifts.R;
+
+/**
+ * Created by Nezar Saleh on 3/29/2016.
+ */
+public class ScreenSlidePageFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+
+
+}
